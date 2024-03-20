3114ecba52bb1a50cc07ccea98b58404153b4b30
Juan <jmha0604@hotmail.com>
2024-03-20 15:09:59 -0600

    Tercer commit

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -2,13 +2,15 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import {HeaderModule} from '../../header/header.module';
+import { FooterModule } from '../../footer/footer.module';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.page.html',
   styleUrls: ['./home.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule,HeaderModule, FooterModule]
 })
 export class HomePage implements OnInit {
 
